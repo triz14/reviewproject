d912544653db68ae2c16e42309602960d73251b2
Beatriz <beatriznovais1404@gmail.com>
2023-08-17 15:54:47 -0300

    atualização do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,19 @@ import './App.css'
 
 function App() {
   const [usuario, setUsuario] = useState("")
+  const [senha, setSenha] = useState("")
+
+  function handleUsuario(evento) {
+    setUsuario(evento.target.value)
+  }
+
+  function handleSenha(evento) {
+    setSenha(evento.target.value)
+  }
+
+  function handleSubmit() {
+    alert(`Usuário: ${usuario} Senha: ${senha}`)
+  }
 
   return (
     <>
@@ -10,14 +23,14 @@ function App() {
         <h1>Login</h1>
       </header>
       <main>
-        <form action="">
+        <form action="" onSubmit={handleSubmit}>
           <div>
             <label htmlFor="usuario">Usuário</label>
-            <input type="text" id="usuario" placeholder="Digite seu usuário"/>
+            <input type="text" id="usuario" placeholder="Digite seu usuário" onChange={handleUsuario}/>
           </div>
           <div>
             <label htmlFor="senha">Senha</label>
-            <input type="password" id="senha" placeholder="Digite sua senha"/>
+            <input type="password" id="senha" placeholder="Digite sua senha" onChange={handleSenha}/>
           </div>
           <button>Entrar</button>
         </form>
